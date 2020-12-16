33498c6bcc10a52cc24f074764e5054cb655dc23
Monty Williams <monty.williams@gmail.com>
2020-12-16 00:19:11 -0800

    Clarify read_YN test

--- tests/test-read_YN.sh
+++ tests/test-read_YN.sh
@@ -6,66 +6,88 @@
 . ../functions/read_YN.function
 
 printf "==> Testing read_YN.function.\n\n"
-printf "==> First, lets test [y/n] prompts. Type a Y, y, N, or n.\n"
-printf "==> They should not proceed if you hit any other character, inclucing <cr>.\n\n"
 
-if read_YN "Testing no default. Hit y."; then
-    printf "Yes\n"
-else
-    printf "No\n"
-fi
-if read_YN "Testing no default. Hit n."; then
-    printf "Yes\n"
-else
-    printf "No\n"
-fi
+printf "==> First, let's test ${RED}[y/n]${NO_COLOR} prompts. Valid responses are Y, y, N, or n.\n"
+printf "==> It should not proceed if you hit any other character, inclucing <cr>.\n\n"
 
-if read_YN "Testing no default. Hit <cr>. Should loop waiting for y or n."; then
+printf "==> One test to show what is displayed if no question is supplied.\n"
+if read_YN; then
     printf "Yes\n"
 else
     printf "No\n"
 fi
 
-if read_YN "Testing no default. Hit a different character. Should loop waiting for y or n."; then
+printf "\n==> Four tests to check possible responses to ${RED}[y/n]${NO_COLOR} prompts.\n"
+if read_YN "1. Hit y."; then
     printf "Yes\n"
 else
     printf "No\n"
 fi
 
-printf "\n\n==> Now lets test [Y/n] and [y/N] prompts Answer Y, y, N, n, or <cr>.\n"
+if read_YN "2. Hit n."; then
+    printf "Yes\n"
+else
+    printf "No\n"
+fi
+
+if read_YN "3. Hit <cr>. Should loop waiting for y or n."; then
+    printf "Yes\n"
+else
+    printf "No\n"
+fi
+
+if read_YN "4. Hit a different character. Should loop waiting for y or n."; then
+    printf "Yes\n"
+else
+    printf "No\n"
+fi
+
+printf "\n==> Now let's test ${RED}[Y/n]${NO_COLOR} and ${RED}[y/N]${NO_COLOR} prompts. "
+printf "Valid responses are Y, y, N, n, or <cr>.\n"
 printf "==> A <cr> should be the same as hitting the capitalized letter.\n"
-printf "==> They should not proceed if you hit any other character than Y, y, N, n, or <cr>.\n"
+printf "==> It should not proceed if you hit any other character than Y, y, N, n, or <cr>.\n"
 
 # Default to Yes [Y/n] if the user presses enter without giving an answer:
-if read_YN "Testing Y default. Hit y." Y; then
+printf "\n==> Four tests to check possible responses to ${RED}[Y/n]${NO_COLOR} prompts.\n"
+if read_YN "1. Hit y." Y; then
     printf "Yes\n"
 else
     printf "No\n"
 fi
-if read_YN "Testing Y default. Hit n." Y; then
+if read_YN "2. Hit n." Y; then
     printf "Yes\n"
 else
     printf "No\n"
 fi
-
-if read_YN "Testing Y default. Hit <cr>." Y; then
+if read_YN "3. Hit <cr>." Y; then
     printf "Yes\n"
 else
     printf "No\n"
 fi
-if read_YN "Testing N default. Hit <cr>." N; then
-    printf "Yes\n"
-else
-    printf "No\n"
-fi
-
-if read_YN "Hit a different character. Should loop until Y, y, N, n, or <cr>." Y; then
+if read_YN "4. Hit a different character. Should loop until Y, y, N, n, or <cr>." Y; then
     printf "Yes\n"
 else
     printf "No\n"
 fi
 
-if read_YN "Hit a different character. Should loop until Y, y, N, n, or <cr>." N; then
+# Default to No [y/N] if the user presses enter without giving an answer:
+printf "\n==> Four tests to check possible responses to ${RED}[y/N]${NO_COLOR} prompts.\n"
+if read_YN "1. Hit y." N; then
+    printf "Yes\n"
+else
+    printf "No\n"
+fi
+if read_YN "2. Hit n." N; then
+    printf "Yes\n"
+else
+    printf "No\n"
+fi
+if read_YN "3. Hit <cr>." N; then
+    printf "Yes\n"
+else
+    printf "No\n"
+fi
+if read_YN "4. Hit a different character. Should loop until Y, y, N, n, or <cr>." N; then
     printf "Yes\n"
 else
     printf "No\n"
