12b0a8e8e6722a8ab5b47e9de2ed451bf14c8b82
Monty Williams <monty.williams@gmail.com>
2020-12-23 18:03:57 -0800

    Implement explain_functions

---
+++ functions/explain_functions.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Explain any files in the function directory named *.function
+#
+# USAGE:
+#   ./explain_functions.sh
+#
+# NOTES:
+#   Runs the 'help' function in any *.function files that have one
+
+# Make sure we are in the correct directory
+DIRNAME=$(dirname "$0")
+cd $DIRNAME/..
+
+export LC_COLLATE="C"
+source functions/define_colors
+source functions/define_files
+source functions/load_functions
+
+for file in $(ls functions/*.function); do
+    if [ $(rg -cF 'help()' $file) ]; then
+        clear
+        function="$(basename ${file%\.function})"
+        eval $function -h
+        printf "\n"
+    fi
+done
