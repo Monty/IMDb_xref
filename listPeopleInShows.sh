1fa98e63c0744fc0bca40fde8205e9bb907c08e9
Monty Williams <monty.williams@gmail.com>
2021-01-27 17:49:37 -0800

    Improve variable names

--- listPeopleInShows.sh
+++ listPeopleInShows.sh
@@ -44,11 +44,34 @@ function terminate() {
     if [ -n "$DEBUG" ]; then
         printf "\nTerminating: $(basename $0)\n" >&2
         printf "Not removing:\n" >&2
-        printf "$ALL_TERMS $TCONST_LIST $TCONST_TERMS $SHOWS_TERMS\n" >&2
-        printf "$POSSIBLE_MATCHES $MATCH_COUNTS $FINAL_RESULTS\n" >&2
+        cat <<EOT >&2
+$ALL_TERMS
+$TCONST_TERMS
+$SHOWS_TERMS
+$POSSIBLE_MATCHES
+$MATCH_COUNTS
+$ALL_MATCHES
+
+$TCONST_LIST
+$SHOW_NAMES
+$EPISODES_LIST
+$NCONST_LIST
+
+$SHOWS_PL
+$EPISODES_PL
+$EPISODE_NAMES_PL
+$NAMES_PL
+
+$CREDITS_CSV
+$EPISODES_CSV
+$CAST_CSV
+EOT
     else
-        rm -f $ALL_TERMS $TCONST_LIST $TCONST_TERMS $SHOWS_TERMS
-        rm -f $POSSIBLE_MATCHES $MATCH_COUNTS $FINAL_RESULTS
+        rm -f $ALL_TERMS $TCONST_TERMS $SHOWS_TERMS
+        rm -f $POSSIBLE_MATCHES $MATCH_COUNTS $ALL_MATCHES
+        rm -f $TCONST_LIST $SHOW_NAMES $EPISODES_LIST $NCONST_LIST
+        rm -f $SHOWS_PL $EPISODES_PL $EPISODE_NAMES_PL $NAMES_PL
+        rm -f $CREDITS_CSV $EPISODES_CSV $CAST_CSV
     fi
 }
 
@@ -97,46 +120,25 @@ ensurePrerequisites
 
 # Need some tempfiles
 ALL_TERMS=$(mktemp)
-TCONST_LIST=$(mktemp)
 TCONST_TERMS=$(mktemp)
-SHOW_NAMES=$(mktemp)
 SHOWS_TERMS=$(mktemp)
 POSSIBLE_MATCHES=$(mktemp)
 MATCH_COUNTS=$(mktemp)
-FINAL_RESULTS=$(mktemp)
-
-# Required subdirectories
-WORK="secondary"
-mkdir -p $WORK
-
-# ALL_TERMS="$WORK/ALL_TERMS.txt"
-# TCONST_LIST="$WORK/TCONST_LIST.txt"
-# TCONST_TERMS="$WORK/TCONST_TERMS.txt"
-# SHOWS_TERMS="$WORK/SHOWS_TERMS.txt"
-# POSSIBLE_MATCHES="$WORK/POSSIBLE_MATCHES.txt"
-# MATCH_COUNTS="$WORK/MATCH_COUNTS.txt"
-# FINAL_RESULTS="$WORK/FINAL_RESULTS.txt"
-
-CREDITS_PERSON="$WORK/Credits-Person.csv"
-# SHOW_NAMES="$WORK/show_names.txt"
-
-TEMP_AWK="$WORK/conflicts.awk"
-EPISODES_LIST="$WORK/tconst-episodes.txt"
-NCONST_LIST="$WORK/nconst.txt"
-TEMP_SHOWS="$WORK/temp_shows.csv"
-UNSORTED_CREDITS="$WORK/unsorted_credits.csv"
-UNSORTED_EPISODES="$WORK/unsorted_episodes.csv"
-
-TCONST_SHOWS_PL="$WORK/tconst-shows-pl.txt"
-TCONST_EPISODES_PL="$WORK/tconst-episodes-pl.txt"
-TCONST_EPISODE_NAMES_PL="$WORK/tconst-episode_names-pl.txt"
-NCONST_PL="$WORK/nconst-pl.txt"
-
-ALL_CSV="$UNSORTED_EPISODES $UNSORTED_CREDITS"
-ALL_OTHERS="$CREDITS_PERSON $SHOW_NAMES $TEMP_AWK $TCONST_LIST "
-ALL_OTHERS+="$EPISODES_LIST $NCONST_LIST $TEMP_SHOWS"
-
-rm -f $ALL_CSV $ALL_OTHERS
+ALL_MATCHES=$(mktemp)
+#
+TCONST_LIST=$(mktemp)
+SHOW_NAMES=$(mktemp)
+EPISODES_LIST=$(mktemp)
+NCONST_LIST=$(mktemp)
+#
+SHOWS_PL=$(mktemp)
+EPISODES_PL=$(mktemp)
+EPISODE_NAMES_PL=$(mktemp)
+NAMES_PL=$(mktemp)
+#
+CREDITS_CSV=$(mktemp)
+EPISODES_CSV=$(mktemp)
+CAST_CSV=$(mktemp)
 
 # Make sure a search term is supplied
 if [ $# -eq 0 ]; then
@@ -189,7 +191,7 @@ while read -r line; do
     count=$(cut -f 1 <<<"$line")
     match=$(cut -f 2 <<<"$line")
     if [ "$count" -eq 1 ]; then
-        rg "\t$match\t" $POSSIBLE_MATCHES >>$FINAL_RESULTS
+        rg "\t$match\t" $POSSIBLE_MATCHES >>$ALL_MATCHES
         continue
     fi
     cat <<EOF
@@ -224,7 +226,7 @@ EOF
                 ;;
             *)
                 printf "Adding: $pickMenu\n"
-                printf "$pickMenu\n" >>$FINAL_RESULTS
+                printf "$pickMenu\n" >>$ALL_MATCHES
                 break
                 ;;
             esac
@@ -237,7 +239,7 @@ done <$MATCH_COUNTS
 printf "\n"
 
 # Didn't find any results
-if [ ! -s "$FINAL_RESULTS" ]; then
+if [ ! -s "$ALL_MATCHES" ]; then
     printf "==> Didn't find ${RED}any${NO_COLOR} matching shows.\n"
     printf "    Check the \"Searching $numRecords records for:\" section above.\n\n"
     loopOrExitP
@@ -246,32 +248,32 @@ fi
 # Found results, check with user before adding to local data
 printf "These are the matches I found:\n"
 if checkForExecutable -q xsv; then
-    sort -f --field-separator=$'\t' --key=3 $FINAL_RESULTS | xsv table -d "\t"
+    sort -f --field-separator=$'\t' --key=3 $ALL_MATCHES | xsv table -d "\t"
 else
-    sort -f --field-separator=$'\t' --key=3 $FINAL_RESULTS
+    sort -f --field-separator=$'\t' --key=3 $ALL_MATCHES
 fi
 ! waitUntil $ynPref -Y && loopOrExitP
 printf "\n"
 
 # Build the lists we need
-rg -N "^tt" $FINAL_RESULTS | cut -f 1 >$TCONST_LIST
-rg -N "^tt" $FINAL_RESULTS | cut -f 3 | sort -f >$SHOW_NAMES
+rg -N "^tt" $ALL_MATCHES | cut -f 1 >$TCONST_LIST
+rg -N "^tt" $ALL_MATCHES | cut -f 3 | sort -f >$SHOW_NAMES
 
 # Create a perl script to GLOBALLY convert a show tconst to a show title
 rg -wNz -f $TCONST_LIST title.basics.tsv.gz |
-    perl -F"\t" -lane 'print "s{\\b@F[0]\\b}\{@F[2]}g;";' >$TCONST_SHOWS_PL
+    perl -F"\t" -lane 'print "s{\\b@F[0]\\b}\{@F[2]}g;";' >$SHOWS_PL
 
 ### Use the tconst list to lookup episode IDs and generate an EPISODE TCONST file
 rg -wNz -f $TCONST_LIST title.episode.tsv.gz |
-    tee $UNSORTED_EPISODES | cut -f 1 >$EPISODES_LIST
+    tee $EPISODES_CSV | cut -f 1 >$EPISODES_LIST
 # Create a perl script to convert an episode tconst to its parent show title
-perl -F"\t" -lane 'print "s{\\b@F[0]\\b}\{@F[1]};";' $UNSORTED_EPISODES |
-    perl -p -f $TCONST_SHOWS_PL >$TCONST_EPISODES_PL
+perl -F"\t" -lane 'print "s{\\b@F[0]\\b}\{@F[1]};";' $EPISODES_CSV |
+    perl -p -f $SHOWS_PL >$EPISODES_PL
 
 # Create a perl script to convert an episode tconst to its episode title
 rg -wNz -f $EPISODES_LIST title.basics.tsv.gz |
     perl -F"\t" -lane 'print "s{\\b@F[0]\\b}\{@F[3]};";' \
-        >$TCONST_EPISODE_NAMES_PL
+        >$EPISODE_NAMES_PL
 
 # Use tconst list to lookup principal titles & generate tconst/nconst credits csv
 # Fix bogus nconst nm0745728, it should be nm0745694. Rearrange fields
@@ -280,42 +282,42 @@ rg -wNz -f $TCONST_LIST title.principals.tsv.gz |
     rg -w -e actor -e actress -e writer -e director -e producer |
     perl -p -e 's+nm0745728+nm0745694+' |
     perl -F"\t" -lane 'printf "%s\t%s\t\t%02d\t%s\t%s\n", @F[2,0,1,3,5]' |
-    tee $UNSORTED_CREDITS | cut -f 1 | sort -u >$NCONST_LIST
+    tee $CREDITS_CSV | cut -f 1 | sort -u >$NCONST_LIST
 
 # Use episodes list to lookup principal titles & add to tconst/nconst credits csv
 # Copy field 1 to the episode title field!
 rg -wNz -f $EPISODES_LIST title.principals.tsv.gz |
     rg -w -e actor -e actress -e writer -e director -e producer |
     perl -F"\t" -lane 'printf "%s\t%s\t%s\t%02d\t%s\t%s\n", @F[2,0,0,1,3,5]' |
-    tee -a $UNSORTED_CREDITS | cut -f 1 | sort -u |
+    tee -a $CREDITS_CSV | cut -f 1 | sort -u |
     rg -v -f $NCONST_LIST >>$NCONST_LIST
 
 # Create a perl script to convert an nconst to a name
 rg -wNz -f $NCONST_LIST name.basics.tsv.gz |
-    perl -F"\t" -lane 'print "s{^@F[0]\\b}\{@F[1]};";' >$NCONST_PL
+    perl -F"\t" -lane 'print "s{^@F[0]\\b}\{@F[1]};";' >$NAMES_PL
 
 # Get rid of ugly \N fields, and unneeded characters. Make sure commas are
 # followed by spaces. Separate multiple characters portrayed with semicolons,
 # remove quotes
-perl -pi -e 's+\\N++g; tr+[]++d; s+,+, +g; s+,  +, +g; s+", "+; +g; tr+"++d;' $UNSORTED_CREDITS
+perl -pi -e 's+\\N++g; tr+[]++d; s+,+, +g; s+,  +, +g; s+", "+; +g; tr+"++d;' $CREDITS_CSV
 
 # Translate tconst and nconst into titles and names
-perl -pi -f $TCONST_SHOWS_PL $UNSORTED_CREDITS
-perl -pi -f $TCONST_EPISODES_PL $UNSORTED_CREDITS
-perl -pi -f $TCONST_EPISODE_NAMES_PL $UNSORTED_CREDITS
-perl -pi -f $NCONST_PL $UNSORTED_CREDITS
+perl -pi -f $SHOWS_PL $CREDITS_CSV
+perl -pi -f $EPISODES_PL $CREDITS_CSV
+perl -pi -f $EPISODE_NAMES_PL $CREDITS_CSV
+perl -pi -f $NAMES_PL $CREDITS_CSV
 
-# Create the sorted CREDITS spreadsheets
+# Create the sorted RESULTS
 printf "Person\tShow Title\tEpisode Title\tRank\tJob\tCharacter Name\n" \
-    >$CREDITS_PERSON
+    >$CAST_CSV
 # Sort by Person (1), Show Title (2), Rank (4), Episode Title (3)
-sort -f --field-separator=$'\t' --key=1,2 --key=4,4 --key=3,3 $UNSORTED_CREDITS \
-    >>$CREDITS_PERSON
+sort -f --field-separator=$'\t' --key=1,2 --key=4,4 --key=3,3 $CREDITS_CSV \
+    >>$CAST_CSV
 
 [ -n "$DEBUG" ] && set -v
 while read -r line; do
     showName="$line"
-    ./xrefCast.sh -f $CREDITS_PERSON -an "$line"
+    ./xrefCast.sh -f $CAST_CSV -an "$line"
     waitUntil -k
 done <$SHOW_NAMES
 
